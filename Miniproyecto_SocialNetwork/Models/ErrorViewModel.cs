dc2f83752c118610da9b0ea923c212e36ad2afa8
dalvinxo <dalvingamer@gmail.com>
2020-07-29 09:16:05 -0400

    Hello Word

---
+++ Miniproyecto_SocialNetwork/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Miniproyecto_SocialNetwork.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
